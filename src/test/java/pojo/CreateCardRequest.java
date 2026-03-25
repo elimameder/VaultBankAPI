1a2000f9dcc60e426e76592fe0b50eaebcc80af1
Aiperi Osmonalieva <aiperi@Aiperis-MacBook-Pro.local>
2026-03-25 12:44:20 -0400

    updated card request and response

--- src/test/java/pojo/CreateCardRequest.java
+++ src/test/java/pojo/CreateCardRequest.java
@@ -9,4 +9,51 @@ public class CreateCardRequest {
     public String card_holder;
     public int credit_limit;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLast4() {
+        return last4;
+    }
+
+    public void setLast4(String last4) {
+        this.last4 = last4;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getExpiry_date() {
+        return expiry_date;
+    }
+
+    public void setExpiry_date(String expiry_date) {
+        this.expiry_date = expiry_date;
+    }
+
+    public String getCard_holder() {
+        return card_holder;
+    }
+
+    public void setCard_holder(String card_holder) {
+        this.card_holder = card_holder;
+    }
+
+    public int getCredit_limit() {
+        return credit_limit;
+    }
+
+    public void setCredit_limit(int credit_limit) {
+        this.credit_limit = credit_limit;
+    }
 }
--- src/test/java/pojo/CreateCardResponse.java
+++ src/test/java/pojo/CreateCardResponse.java
@@ -4,4 +4,20 @@ public class CreateCardResponse {
 
     public String code;
     public String message;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
